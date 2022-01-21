Fix wrong shape in beam decoder

src/transformers/generation_beam_search.py
@@ -208,7 +208,7 @@ class BeamSearchScorer(BeamScorer):
         input_ids: torch.LongTensor,
         next_scores: torch.FloatTensor,
         next_tokens: torch.LongTensor,
-        next_probs: torch.FloatTensor,
+        next_probs: torch.FloatTensor,  # (bs * n_beam, n_vocab)
         next_indices: torch.LongTensor,
         pad_token_id: Optional[int] = None,
         eos_token_id: Optional[int] = None,
@@ -232,6 +232,8 @@ class BeamSearchScorer(BeamScorer):
         next_beam_tokens = torch.zeros((batch_size, self.group_size), dtype=next_tokens.dtype, device=device)
         next_beam_indices = torch.zeros((batch_size, self.group_size), dtype=next_indices.dtype, device=device)
         next_beam_probs = torch.zeros((batch_size, self.group_size, self.vocab_size), dtype=next_probs.dtype, device=device)
+
+        # debug_next_probs = next_probs.view(batch_size, -1)
 
         for batch_idx, beam_hyp in enumerate(self._beam_hyps):
             if self._done[batch_idx]:
@@ -267,16 +269,16 @@ class BeamSearchScorer(BeamScorer):
                     beam_hyp.add(
                         input_ids[batch_beam_idx].clone(),
                         next_score.item(),
-                        next_probs[next_index.item()].clone(),
+                        next_probs[batch_beam_idx].clone(),
                     )
                 else:
-                    start = (next_index.item()) * self.vocab_size
-                    end = start + self.vocab_size
+                    # start = (next_index.item()) * self.vocab_size
+                    # end = start + self.vocab_size
                     # add next predicted token since it is not eos_token
                     next_beam_scores[batch_idx, beam_idx] = next_score
                     next_beam_tokens[batch_idx, beam_idx] = next_token
                     next_beam_indices[batch_idx, beam_idx] = batch_beam_idx
-                    d = next_probs[batch_idx, start:end]
+                    # d = debug_next_probs[batch_idx, start:end]
                     # sanity/debugging checks
                     _ = """
                     k = torch.topk(d, 2 * 3, largest=True, sorted=True)
@@ -288,7 +290,8 @@ class BeamSearchScorer(BeamScorer):
                     print(f"   batch={batch_idx} beam={beam_idx} rank={beam_token_rank} "
                           f"gt={next_token.item()} pred={pred}, mass={round(float(d.sum()), 2)}", k.indices, next_index.item())
                     """
-                    next_beam_probs[batch_idx, beam_idx] = d.clone()
+                    # assert (d == next_probs[batch_beam_idx]).all()
+                    next_beam_probs[batch_idx, beam_idx] = next_probs[batch_beam_idx].clone()
                     beam_idx += 1
 
                 # once the beam for next step is full, don't add more tokens to it.

src/transformers/generation_utils.py
@@ -1973,7 +1973,7 @@ class GenerationMixin:
             vocab_size = next_token_scores.shape[-1]
             next_token_probs = nn.functional.softmax(next_token_scores, dim=1)
             next_token_scores = next_token_scores.view(batch_size, num_beams * vocab_size)
-            next_token_probs = next_token_probs.view(batch_size, num_beams * vocab_size)
+            next_token_probs = next_token_probs.view(batch_size * num_beams, vocab_size)
 
             next_token_scores, next_tokens = torch.topk(
                 next_token_scores, 2 * num_beams, dim=1, largest=True, sorted=True
@@ -2301,7 +2301,7 @@ class GenerationMixin:
                 input_ids,
                 next_token_scores,
                 next_tokens,
-                probs,  # next_token_probs
+                probs.view(batch_size * num_beams, vocab_size),  # next_token_probs
                 next_indices,
                 pad_token_id=pad_token_id,
                 eos_token_id=eos_token_id,
@@ -2616,7 +2616,7 @@ class GenerationMixin:
                 next_token_probs = nn.functional.softmax(next_token_scores, dim=1)
                 # reshape for beam search
                 next_token_scores = next_token_scores.view(batch_size, group_size * vocab_size)
-                next_token_probs = next_token_probs.view(batch_size, group_size * vocab_size)
+                next_token_probs = next_token_probs.view(batch_size * group_size, vocab_size)
 
                 next_token_scores, next_tokens = torch.topk(
                     next_token_scores, 2 * group_size, dim=1, largest=True, sorted=True